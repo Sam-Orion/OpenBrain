d9d01bd064af7ce201a09ec77610456303c3469f
Sam Orion <samorion@Sams-MacBook-Air.local>
2026-03-20 20:24:06 +0530

    remove config.ts from git tracking

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export const JWT_PASSWORD =
-  "icm,Nc*V{a)#6A06ECp_Dnc$O[%&G,;uKBOY[k.tJ^ycSzV2#Ah(2Ffe+wecvD8sS5,#mKEfx|SRi6yr[$fhhY";
-
-export const MONGODB_CONN_STRING =
-  "mongodb+srv://admin:xonxik-cenWyj-6sisku@cluster0.rqmw0km.mongodb.net/OpenBrain";
